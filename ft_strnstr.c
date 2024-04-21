08bad35057def2ae8f36f9d19dbda1c5d6ccd5ea
Asmae Tahtouh <atahtouh@e1r1p11.1337.ma>
2024-04-21 22:26:06 +0100

    libft

---
+++ ft_strnstr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahtouh <atahtouh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 09:11:04 by atahtouh          #+#    #+#             */
+/*   Updated: 2023/11/20 21:36:51 by atahtouh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (haystack == needle || needle[0] == '\0')
+		return ((char *)haystack);
+	if (len == 0)
+		return (NULL);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (needle[j] != '\0' && i + j < len)
+		{
+			if (needle[j] != haystack[i + j])
+				break ;
+		j++;
+		}
+		if (needle[j] == '\0')
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
+}
+/*int main()
+{
+	char *haystack = "abcdefgh";
+	char *needle = "abc";
+	printf("%s",ft_strnstr(haystack,needle,3));
+}*/
